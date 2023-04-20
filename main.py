7c9b866bce3b6b742519b348807595be70a58d8b
dotan <frenkeldotan1998@gmail.com>
2023-04-20 11:32:49 +0300

    Add - add new student option

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import sys
 from PyQt6.QtWidgets import QApplication, QVBoxLayout, QLabel, \
-     QWidget, QGridLayout, QLineEdit, QPushButton, QMainWindow
-from  PyQt6.QtGui import QAction
+     QWidget, QGridLayout, QLineEdit, QPushButton, QMainWindow, \
+     QTableWidget, QTableWidgetItem, QDialog, QComboBox
+from PyQt6.QtGui import QAction
+import sqlite3
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -14,16 +16,83 @@ class MainWindow(QMainWindow):
 
         # Sub bar
         add_student_action = QAction("Add Student", self)
+        add_student_action.triggered.connect(self.insert)
         file_menu_item.addAction(add_student_action)
 
         about_action = QAction("About", self)
         help_menu_item.addAction(about_action)
 
+        # Table of data
+        self.table = QTableWidget()
+        self.table.setColumnCount(4)
+        self.table.setHorizontalHeaderLabels(("Id", "Name", "Course", "Mobile"))
+        self.table.verticalHeader().setVisible(False)   # removes the numbering of the table, because i have ID in my data
+        self.setCentralWidget(self.table)
 
 
+    def load_data(self):
+        connection = sqlite3.connect("database.db")
+        result = connection.execute("SELECT * FROM students")
+        self.table.setRowCount(0)   # this prevents duplicate data, this make sure that each time which load the data the row start from 0
+        for row_number, row_data in enumerate(result):
+            self.table.insertRow(row_number)
+            for column_number, data in enumerate(row_data):
+                self.table.setItem(row_number, column_number, QTableWidgetItem(str(data)))
+        connection.close()
 
+    def insert(self):
+        dialog = InsertDialog()
+        dialog.exec()
+
+
+class InsertDialog(QDialog):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Insert Student Data")
+        self.setFixedWidth(300)
+        self.setFixedHeight(300)
+
+        layout = QVBoxLayout()
+
+        # Add student name widget
+        self.student_name = QLineEdit()
+        self.student_name.setPlaceholderText("Name")
+        layout.addWidget(self.student_name)
+
+        # Add combo box of courses
+        self.course = QComboBox()
+        courses = ["Biology", "Math", "Astronomy", "Physics"]
+        self.course.addItems(courses)
+        layout.addWidget(self.course)
+
+        # Add student phone number
+        self.mobile = QLineEdit()
+        self.mobile.setPlaceholderText("Mobile")
+        layout.addWidget(self.mobile)
+
+        # Add submit button
+        button = QPushButton("Submit")
+        button.clicked.connect(self.add_student)
+        layout.addWidget(button)
+
+        self.setLayout(layout)
+
+    def add_student(self):
+        name = self.student_name.text()
+        course = self.course.itemText(self.course.currentIndex())
+        mobile = self.mobile.text()
+        connection = sqlite3.connect("database.db")
+        cursor = connection.cursor()
+        cursor.execute("INSERT INTO students (name, course, mobile) VALUES (?, ?, ?)",
+                       (name, course, mobile))
+
+        connection.commit()
+        cursor.close()
+        connection.close()
+        main_window.load_data()   # refreshes the window
 
 app = QApplication(sys.argv)
-student_data = MainWindow()
-student_data.show()
+main_window = MainWindow()
+main_window.show()
+main_window.load_data()
 sys.exit(app.exec())
